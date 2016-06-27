b8f07907edce4ab1fff24f782f4b8989a420fefc
David Little <david.frank.little@gmail.com>
2016-06-27 11:05:20 -0500

    Assignment mode now correctly updates when modifying the document.

--- app/reducers/assign_mode.js
+++ app/reducers/assign_mode.js
@@ -1,8 +1,29 @@
-import {ASSIGN_MODE, STANDARD} from './commands'
+import {ASSIGN_MODE, DOCUMENT, STANDARD, STUDENT,
+        COURSE, REMOVE, CHANGE, ASSIGN} from './commands'
 
 export default function assign_mode(state = {mode: STANDARD},action){
   switch(action.type){
     case ASSIGN_MODE: return {mode: action.mode, id: action.id}
+    case DOCUMENT:
+      // if we change the student name, update the assignment state
+      if(state.mode === STUDENT && action.field == STUDENT){
+
+        // reset mode if the assigning student was removed
+        if(action.command === REMOVE && action.id === state.id)
+          return {mode: STANDARD}
+
+        // update the mode if the assigning student's name was changed
+        if(action.command === CHANGE && action.subfield[0] === 'name' &&
+           action.id === state.id)
+          return {mode: STUDENT, id: action.to}
+
+      }else if(state.mode === COURSE && action.field === COURSE){
+
+        // reset mode if the assigning course was removed
+        if(action.command === REMOVE && action.id === state.id)
+          return {mode: STANDARD}
+      }
+      return state
     default: return state
   }
 }
