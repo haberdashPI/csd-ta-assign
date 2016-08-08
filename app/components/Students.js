d4f2ef2a75c1a8601e612c69b70a4f55e8d56ae3
David Little <david.frank.little@gmail.com>
2016-08-08 16:48:04 -0500

    Fixed bug in display of fixed assignment button.

--- app/components/Students.js
+++ app/components/Students.js
@@ -439,7 +439,7 @@ class _Assignments extends Component{
                    <Glyphicon glyph="minus"/>
                  </Button>
                  <Button bsSize="xsmall" disabled={disabled}
-                         bsStyle={(assign.get('fix') ? "primary" : "")}
+                         bsStyle={(assign.get('fix') ? "primary" : "default")}
                          onClick={to => onFix(name,cid,!assign.get('fix'))}
                          active={assign.get('fix')}>
                    <Glyphicon glyph="magnet"/>
